77dc6241d5fde0e0f79379f5ba531af3a93ea4a1
Ge Yang <yangge1987@gmail.com>
2016-06-27 23:12:24 -0700

    BUILD DOC

---
+++ Readme.js
@@ -0,0 +1,66 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Readme;
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactMarkdownit = require("react-markdownit");
+
+var _reactMarkdownit2 = _interopRequireDefault(_reactMarkdownit);
+
+var _reactHighlight = require("react-highlight.js");
+
+var _reactHighlight2 = _interopRequireDefault(_reactHighlight);
+
+var _reactComponentPropsTable = require("react-component-props-table");
+
+var _reactComponentPropsTable2 = _interopRequireDefault(_reactComponentPropsTable);
+
+var _HappySandwichMaker = require("../HappySandwichMaker.example");
+
+var _HappySandwichMaker2 = _interopRequireDefault(_HappySandwichMaker);
+
+var _HappySandwichMaker3 = require("!!raw!../HappySandwichMaker.example");
+
+var _HappySandwichMaker4 = _interopRequireDefault(_HappySandwichMaker3);
+
+var _HappySandwichMaker5 = require("!!react-docgen!../HappySandwichMaker");
+
+var _HappySandwichMaker6 = _interopRequireDefault(_HappySandwichMaker5);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _objectDestructuringEmpty(obj) { if (obj == null) throw new TypeError("Cannot destructure undefined"); } /**
+                                                                                                                   * Created by ge on 6/24/16.
+                                                                                                                   */
+
+
+function Readme(_ref) {
+  _objectDestructuringEmpty(_ref);
+
+  return _react2.default.createElement(
+    _reactMarkdownit2.default,
+    { stripIndent: true },
+    "\n      # React ES6 Component Template\n\n      [![github](https://img.shields.io/github/downloads/episodeyang/react-es6-template/total.svg?style=flat-square&maxAge=2592000)]()\n\n      A template repo for react components written with es6 syntax.\n      ## Usage\n\n      ~~~shell\n      git clone https://github.com/episodeyang/react-es6-template\n      ~~~\n\n      After cloning from gitHub, you can run the example by doing\n      ~~~shell\n      npm run serve-docs\n      ~~~\n\n      And then open your browser at [http://localhost:5000](http://localhost:5000).\n\n      This one calls webpack (you should look at the `package.json` source) and uses the\n      webpack-dev-server to serve from `./src/example/` with hot module reloading.\n\n      ### How is this README written:\n      This readme is written with react and markdown. It includes:\n      1. a **live react component demo**\n      2. a table of the component's props that is generated automatically\n      3. **source** of the example component\n\n      ## Example Component: `HappySandwichMaker`\n      This component makes you a delicious Subway sandwich.\n      ",
+    _react2.default.createElement(_HappySandwichMaker2.default, null),
+    "### Props",
+    "This table below is generated automatically",
+    _react2.default.createElement(
+      "div",
+      { className: "table-container horizontal-scroll flex-column center" },
+      _react2.default.createElement(_reactComponentPropsTable2.default, { propMetaData: _HappySandwichMaker6.default.props })
+    ),
+    "\n      ### Usage Example\n\n      The source code below of the example above is loaded using the webpack raw loader.",
+    _react2.default.createElement(
+      _reactHighlight2.default,
+      null,
+      _HappySandwichMaker4.default
+    ),
+    "\n      ## Develop\n\n      1. First make your changes, then git commit. Use `serve-docs` to view live update at [http://localhost:5000](http://localhost:5000).\n      2. run `build-docs`, `build-static-docs`, `gh-pages`\n      3. Then remember to push to master.\n\n      "
+  );
+}
--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-/**
- * Created by ge on 6/23/16.
- */
 "use strict";
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require("react-dom");
+
+var _Readme = require("./Readme");
+
+var _Readme2 = _interopRequireDefault(_Readme);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  (0, _reactDom.render)(_react2.default.createElement(_Readme2.default, null), document.body.querySelector('#readme-container'));
+}); /**
+     * Created by ge on 6/23/16.
+     */
