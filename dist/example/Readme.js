69944eab88ff97bd313fbe9d4c9c145c3b627089
Ge Yang <yangge1987@gmail.com>
2016-06-29 00:49:09 -0700

    BUILD DOC

--- dist/example/Readme.js
+++ dist/example/Readme.js
@@ -13,7 +13,7 @@ var _reactMarkdownit = require("react-markdownit");
 
 var _reactMarkdownit2 = _interopRequireDefault(_reactMarkdownit);
 
-var _reactHighlight = require("react-highlight.js");
+var _reactHighlight = require("@episodeyang/react-highlight.js");
 
 var _reactHighlight2 = _interopRequireDefault(_reactHighlight);
 
@@ -46,7 +46,7 @@ function Readme(_ref) {
   return _react2.default.createElement(
     _reactMarkdownit2.default,
     { stripIndent: true },
-    "\n      # React ES6 Component Template\n\n      [![github](https://img.shields.io/github/downloads/episodeyang/react-es6-template/total.svg?style=flat-square&maxAge=2592000)]()\n\n      A template repo for react components written with es6 syntax.\n      ## Usage\n\n      ~~~shell\n      git clone https://github.com/episodeyang/react-es6-template\n      ~~~\n\n      After cloning from gitHub, you can run the example by doing\n      ~~~shell\n      npm run serve-docs\n      ~~~\n\n      And then open your browser at [http://localhost:5000](http://localhost:5000).\n\n      This one calls webpack (you should look at the `package.json` source) and uses the\n      webpack-dev-server to serve from `./src/example/` with hot module reloading.\n\n      ### How is this README written:\n      This readme is written with react and markdown. It includes:\n      1. a **live react component demo**\n      2. a table of the component's props that is generated automatically\n      3. **source** of the example component\n\n      ## Example Component: `HappySandwichMaker`\n      This component makes you a delicious Subway sandwich.\n      ",
+    "\n      # React ES6 Component Template\n\n      [![github](https://img.shields.io/github/downloads/episodeyang/react-es6-template/total.svg?style=flat-square&maxAge=2592000)]()\n\n      A template repo for react components written with es6 syntax.\n\n      ## Usage (as a template)\n\n      ~~~shell\n      git clone https://github.com/episodeyang/react-es6-template <name of your new component>\n      cd <name of your new component>\n      rm -rf .git\n      rm -rf dist\n\n      # now do your git init, git add/coommit/push\n      # replace \"react-es6-template\" with your own\n      # replace \"react es6 template\" with your own\n      # replace \"react es6 component\" with you own etc.\n      ~~~\n\n      After cloning from gitHub, you can run the example by doing\n      ~~~shell\n      npm run serve-docs\n      ~~~\n\n      And then open your browser at [http://localhost:5000](http://localhost:5000).\n\n      This one calls webpack (you should look at the `package.json` source) and uses the\n      webpack-dev-server to serve from `./src/example/` with hot module reloading.\n\n      ### How is this README written:\n      This readme is written with react and markdown. It includes:\n      1. a **live react component demo**\n      2. a table of the component's props that is generated automatically\n      3. **source** of the example component\n\n      ## Example Component: `HappySandwichMaker`\n      This component makes you a delicious Subway sandwich.\n      ",
     _react2.default.createElement(_HappySandwichMaker2.default, null),
     "### Props",
     "This table below is generated automatically",
